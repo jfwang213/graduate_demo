79c7eeba5993eb75433d12201d5a2222f3abe12d
Jianfei Wang <jfwang213@gmail.com>
2011-07-18 22:05:15 +0800

    control transceiver is ok

---
+++ control/transceiver.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+from gnuradio import gr, gru, modulation_utils
+from gnuradio import usrp
+from gnuradio import eng_notation
+from gnuradio.eng_option import eng_option
+from optparse import OptionParser
+
+import random
+import struct
+import sys
+import time
+
+# from current dir
+import usrp_receive_path, usrp_transmit_path
+
+
+
+class my_top_block(gr.top_block):
+    def __init__(self, demodulator, modulator, rx_callback, options):
+        gr.top_block.__init__(self)
+
+        self.rxpath = usrp_receive_path.usrp_receive_path(demodulator, rx_callback, options)
+
+        self.connect(self.rxpath)
+
+        self.txpath = usrp_transmit_path.usrp_transmit_path(modulator, options)
+
+        self.connect(self.txpath)
+
+
+def main():
+    
+    def send_pkt(payload='', eof=False):
+        return tb.txpath.send_pkt(payload, eof)
+
+    
+    global n_rcvd, n_right
+
+    n_rcvd = 0
+    n_right = 0
+    
+    def rx_callback(ok, payload):
+        global n_rcvd, n_right
+        (pktno,) = struct.unpack('!H', payload[0:2])
+        n_rcvd += 1
+        if ok:
+            n_right += 1
+
+        print "ok = %5s  pktno = %4d  n_rcvd = %4d  n_right = %4d" % (
+            ok, pktno, n_rcvd, n_right)
+
+
+    demods = modulation_utils.type_1_demods()
+    
+    mods = modulation_utils.type_1_mods()
+
+    parser = OptionParser(option_class=eng_option, conflict_handler="resolve")
+    expert_grp = parser.add_option_group("Expert")
+
+    parser.add_option("-m", "--modulation", type="choice", choices=mods.keys(),
+                      default='gmsk',
+                      help="Select modulation from: %s [default=%%default]"
+                            % (', '.join(mods.keys()),))
+
+    parser.add_option("-s", "--size", type="eng_float", default=1500,
+                      help="set packet size [default=%default]")
+    parser.add_option("-M", "--megabytes", type="eng_float", default=1.0,
+                      help="set megabytes to transmit [default=%default]")
+    parser.add_option("","--discontinuous", action="store_true", default=False,
+                      help="enable discontinous transmission (bursts of 5 packets)")
+    parser.add_option("","--from-file", default=None,
+                      help="use file for packet contents")
+
+    usrp_transmit_path.add_options(parser, expert_grp)
+    usrp_receive_path.add_options(parser, expert_grp)
+    for mod in mods.values():
+        mod.add_options(expert_grp)
+
+    for demod in demods.values():
+        demod.add_options(expert_grp)
+
+    (options, args) = parser.parse_args ()
+
+    if len(args) != 0:
+        parser.print_help()
+        sys.exit(1)
+
+    if options.tx_freq is None or options.rx_freq is None:
+        sys.stderr.write("You must specify -f FREQ or --freq FREQ\n")
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    tb = my_top_block(demods[options.modulation], mods[options.modulation], rx_callback, options)
+
+    r = gr.enable_realtime_scheduling()
+    if r != gr.RT_OK:
+        print 'Warning: Failed to enable realtime scheduling.'
+
+    tb.start()
+    # generate and send packets
+    nbytes = int(1e6 * options.megabytes)
+    n = 0
+    pktno = 0
+    pkt_size = int(options.size)
+
+    while n < nbytes:
+        if options.from_file is None:
+            data = (pkt_size - 2) * chr(pktno & 0xff) 
+        else:
+            data = source_file.read(pkt_size - 2)
+            if data == '':
+                break;
+
+        payload = struct.pack('!H', pktno & 0xffff) + data
+        send_pkt(payload)
+        n += len(payload)
+        sys.stderr.write('.')
+        time.sleep(0.01)	
+        if options.discontinuous and pktno % 5 == 4:
+            time.sleep(1)
+        pktno += 1
+     
+    send_pkt('',True)
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
+
+
