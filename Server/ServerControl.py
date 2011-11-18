20bd1b30d7723eddc6b3bc54d6b2e4ea37727547
root <root@vrlab726-desktop.(none)>
2011-11-18 21:25:01 +0800

    improve server Control

--- Server/ServerControl.py
+++ Server/ServerControl.py
@@ -77,8 +77,8 @@ class ServerControl(object):
     def __init__(self):
         self.macAddress = 1 #server mac address
         self.clients = {}
-        self.ratio = 1
-        
+        #self.ratio = 1
+        self.ratio = 0.5 
         self.tr = transceiver(self.ReceivePacket)
         self.serverDataChannelFree = []
         self.serverDataChannelActive = {} #{mac: socket}
